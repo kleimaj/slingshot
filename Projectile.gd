8ee20f0aac5bc37fdf0d71bfd76f56d59648a7ba
kleimaj <jacobakleiman@gmail.com>
2021-02-12 13:54:54 -0800

    initializes Projectile released signal

--- Projectile.gd
+++ Projectile.gd
@@ -3,12 +3,17 @@ extends RigidBody2D
 # Boolean dragging the Projectile or not
 var drag_enabled = false
 
+# Custom signal to apply force to the character (release the slingshot)
+signal released
+
 # drag_enabled changes when left mouse button is clicked
 func _input_event(viewport, event, shape_idx):
 	if mode == MODE_STATIC:
 		if event is InputEventMouseButton:
 			if event.button_index == BUTTON_LEFT:
 				drag_enabled = event.pressed
+				if (not drag_enabled):
+					emit_signal("released")
 
 # moves projectile with cursor on drag_enabled
 func _physics_process(delta):
--- Slingshot.gd
+++ Slingshot.gd
@@ -9,4 +9,9 @@ var on_slingshot = true
 func _process(delta):
 	if on_slingshot:
 		var projectile_relative_position = projectile.position - position
+		# The point of the line with index 0 is fixed in the starting position of Projectile; to update the line we only need to update point 1, setting it to the relative position of the projectile.
 		points[1] = projectile_relative_position
+
+
+func _on_Projectile_released():
+	pass # Replace with function body.
