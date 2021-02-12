d10eaf51c1e4df0fa7bbbc10edb8bf0f11d9066f
kleimaj <jacobakleiman@gmail.com>
2021-02-12 13:33:59 -0800

    adds Pickable Projectile

--- Projectile.gd
+++ Projectile.gd
@@ -1,2 +1,16 @@
 extends RigidBody2D
 
+# Boolean dragging the Projectile or not
+var drag_enabled = false
+
+# drag_enabled changes when left mouse button is clicked
+func _input_event(viewport, event, shape_idx):
+	if mode == MODE_STATIC:
+		if event is InputEventMouseButton:
+			if event.button_index == BUTTON_LEFT:
+				drag_enabled = event.pressed
+
+# moves projectile with cursor on drag_enabled
+func _physics_process(delta):
+	if drag_enabled:
+		position = get_global_mouse_position()
