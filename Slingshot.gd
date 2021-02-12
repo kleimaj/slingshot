33a23d600c9a956a8533628d36e72adafb6e3f55
kleimaj <jacobakleiman@gmail.com>
2021-02-12 13:50:09 -0800

    draws line2d when projectile is dragged

--- Slingshot.gd
+++ Slingshot.gd
@@ -1,2 +1,12 @@
 extends Line2D
 
+onready var projectile = $"../Projectile"
+onready var camera = $"../Projectile/Camera2D"
+
+# Boolean is Projectile on Slingshot (initial value true)
+var on_slingshot = true
+
+func _process(delta):
+	if on_slingshot:
+		var projectile_relative_position = projectile.position - position
+		points[1] = projectile_relative_position
