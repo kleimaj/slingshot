2f0f19ad5fd6b1b2bd1d669812df1b1c0cf43a79
kleimaj <jacobakleiman@gmail.com>
2021-02-15 16:35:32 -0800

    implents launch projectile

--- Objects/Slingshot.gd
+++ Objects/Slingshot.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
 var current_projectile = null
-onready var projectiles = get_tree().get_nodes_in_group("projectile")
+onready var projectiles = get_tree().get_nodes_in_group("projectiles")
 
 func load_projectile():
 	if len(projectiles) > 0:
 		current_projectile = projectiles.pop_back()
+		current_projectile.global_position = $RestPosition.global_position
 
-func launch_projectile(proj, launch_impulse):
-	pass
+func launch_projectile(proj: RigidBody2D, launch_impulse):
+	proj.mode = RigidBody2D.MODE_RIGID
+	proj.apply_central_impulse(launch_impulse)
+	current_projectile = null
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
-		if event.pressed and event.button_index == BUTTON_LEFT:
-			load_projectile()
+		if event.pressed:
+			if current_projectile:
+				launch_projectile(current_projectile, Vector2(200, -100))
+			else:
+				load_projectile()
