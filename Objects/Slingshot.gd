101f9acca50502685f7490e8940f7c9e1399bc51
kleimaj <jacobakleiman@gmail.com>
2021-02-15 18:48:39 -0800

    implements drag and launch projectile

--- Objects/Slingshot.gd
+++ Objects/Slingshot.gd
@@ -1,7 +1,15 @@
 extends Node2D
+const MAX_DISTANCE = 150
+
+export var ELASTIC_FORCE = 5
 
 var current_projectile = null
+var launch_force = Vector2()
 onready var projectiles = get_tree().get_nodes_in_group("projectiles")
+onready var rest = $RestPosition
+
+func _ready():
+	load_projectile()
 
 func load_projectile():
 	if len(projectiles) > 0:
@@ -10,13 +18,26 @@ func load_projectile():
 
 func launch_projectile(proj: RigidBody2D, launch_impulse):
 	proj.mode = RigidBody2D.MODE_RIGID
-	proj.apply_central_impulse(launch_impulse)
+	proj.apply_central_impulse(launch_impulse * ELASTIC_FORCE)
 	current_projectile = null
 
 func _unhandled_input(event):
-	if event is InputEventMouseButton:
+	if current_projectile == null:
+		return
+	if event is InputEventScreenTouch:
 		if event.pressed:
-			if current_projectile:
-				launch_projectile(current_projectile, Vector2(200, -100))
-			else:
-				load_projectile()
+			_on_touch_pressed(event)
+		else:
+			_on_touch_released(event)
+	if event is InputEventScreenDrag:
+		_on_touch_drag(event)
+
+func _on_touch_pressed(event):
+	print("pressed", event.position)
+
+func _on_touch_released(event):
+	launch_projectile(current_projectile, launch_force)
+
+func _on_touch_drag(event):
+	launch_force = (rest.global_position - event.position).clamped(MAX_DISTANCE)
+	current_projectile.position = rest.global_position - launch_force
