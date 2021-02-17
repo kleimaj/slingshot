0ec2e3aed82fe2e2d744c36b547aef8988b643ed
kleimaj <jacobakleiman@gmail.com>
2021-02-17 12:27:49 -0800

    adds collision instensity calculation to Enemy.gd

--- Objects/Enemy.gd
+++ Objects/Enemy.gd
@@ -1,6 +1,21 @@
 extends RigidBody2D
 
+export var DESTRUCTION_THRESHOLD = 250
 
 
 func _on_Enemy_body_entered(body):
-	pass # Replace with function body.
+	if body is RigidBody2D:
+		_on_rigid_body_collision(body)
+
+func _on_rigid_body_collision(collider):
+	# calculate momentum (M = v * mass)
+	var collider_momentum = collider.linear_velocity * collider.mass
+	var enemy_momentum = linear_velocity * mass
+	var collision_intensity = (collider_momentum - enemy_momentum).length()
+	if collision_intensity > DESTRUCTION_THRESHOLD:
+		queue_free()
+		collider.queue_free()
+	
+
+#func _integrate_forces(state):
+#	pass
