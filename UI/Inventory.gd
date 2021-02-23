7d6a4c6e24a930d1d2511c6c64e9f98bfcf1faf7
kleimaj <jacobakleiman@gmail.com>
2021-02-23 12:32:03 -0800

    implements default selected pill

--- UI/Inventory.gd
+++ UI/Inventory.gd
@@ -1,2 +1,12 @@
 extends Node2D
 
+var selected_pill = null
+var selected_idx = 0
+onready var grid = $GridContainer
+
+func _ready():
+	#	Show the default selected pill (pill 0)
+	grid.get_child(selected_idx).get_child(0).show()
+
+func get_pill():
+	return selected_pill
