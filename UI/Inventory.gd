59e0368d4bd2904fe88d7c355bb092e5bff299c7
kleimaj <jacobakleiman@gmail.com>
2021-02-23 13:20:08 -0800

    adds Selected inventory functionality

--- UI/Inventory.gd
+++ UI/Inventory.gd
@@ -4,9 +4,34 @@ var selected_pill = null
 var selected_idx = 0
 onready var grid = $GridContainer
 
+var map = {}
+
 func _ready():
 	#	Show the default selected pill (pill 0)
-	grid.get_child(selected_idx).get_child(0).show()
+	#	Assign selected_pill (pill 0)
+	set_selected()
+	# attach gui input listeners
+	for slot in grid.get_children():
+		slot.connect("gui_input", self, "slot_gui_input", [slot])
+		# create map
+		map[slot.name] = map.size()
+		
+func slot_gui_input(event: InputEvent, slot):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT and event.pressed:
+			#	hide currently selected pill
+			hide_selected()
+			#	Get new idx
+			selected_idx = map[slot.name]
+			# Set new selected, set new pill
+			set_selected()
 
 func get_pill():
 	return selected_pill
+
+func hide_selected():
+	grid.get_child(selected_idx).get_child(0).hide()
+
+func set_selected():
+	grid.get_child(selected_idx).get_child(0).show()
+	selected_pill = grid.get_child(selected_idx).get_child(1)
