ccad0c646b5afd83ca0ea3ef55d7b83f9c2803e2
kleimaj <jacobakleiman@gmail.com>
2021-02-23 13:41:51 -0800

    adds book texture rect with button functionality

--- UI/UI.gd
+++ UI/UI.gd
@@ -1,13 +1,26 @@
 extends Control
 
 var inventoryIsOpen = false
+var bookIsOpen = false
 
 func _ready():
 	set_inventory()
+	set_book()
 
 func _on_BackpackButton_pressed():
 	inventoryIsOpen = !inventoryIsOpen
+	bookIsOpen = false
 	set_inventory()
+
+func _on_BookButton_pressed():
+	bookIsOpen = !bookIsOpen
+	inventoryIsOpen = false
+	set_book()
 
 func set_inventory():
 	$CanvasLayer/Inventory.visible = inventoryIsOpen
+
+func set_book():
+	$CanvasLayer/Book.visible = bookIsOpen
+
+
