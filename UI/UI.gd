d9415f87fbc2f89609ef851c1a1e891e3e0b3644
kleimaj <jacobakleiman@gmail.com>
2021-02-23 13:36:28 -0800

    adds texture buttons to toggle inventory

--- UI/UI.gd
+++ UI/UI.gd
@@ -1,3 +1,13 @@
 extends Control
 
 var inventoryIsOpen = false
+
+func _ready():
+	set_inventory()
+
+func _on_BackpackButton_pressed():
+	inventoryIsOpen = !inventoryIsOpen
+	set_inventory()
+
+func set_inventory():
+	$CanvasLayer/Inventory.visible = inventoryIsOpen
