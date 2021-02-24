4129b998be5d97b855e0901218a8d5fa42bea760
kleimaj <jacobakleiman@gmail.com>
2021-02-24 10:50:15 -0800

    changes UI anchors, builds project

--- UI/UI.gd
+++ UI/UI.gd
@@ -10,11 +10,13 @@ func _ready():
 func _on_BackpackButton_pressed():
 	inventoryIsOpen = !inventoryIsOpen
 	bookIsOpen = false
+	set_book()
 	set_inventory()
 
 func _on_BookButton_pressed():
 	bookIsOpen = !bookIsOpen
 	inventoryIsOpen = false
+	set_inventory()
 	set_book()
 
 func set_inventory():
